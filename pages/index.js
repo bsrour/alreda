d2fe5de4bffd86aabc6e45cf54d52147a1df876b
bsrour <srour.b@gmail.com>
2021-03-15 09:57:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,67 @@
-{
-"HorizonLayout":[
-{
-"showSearch":true,
-"showMenu":true,
-"image":"https://a-maash.com/logo.png",
-"showLogo":true,
-"layout":"logo"
-},
-{
-"layout":"bannerImage",
-"items":[
-{
-"padding":15,
-"radius":0,
-"category":null,
-"image":"https://a-maash.com/wp-content/uploads/2021/02/129712943_233890471431724_2145683483490355597_n-300x114.jpg"
+
+
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
 }
-],
-"isSlider":false
-},
-{
-"layout":"bannerImage",
-"design":"default",
-"autoPlay":false,
-"radius":10,
-"items":[
-{
-"category":44,
-"radius":0,
-"image":"https://a-maash.com/wp-content/uploads/2021/02/App_Icon-2-300x300.png",
-"padding":15
-},
-{
-"image":"https://a-maash.com/wp-content/uploads/2021/02/App_Icon-2-300x300.png",
-"category":8,
-"padding":15,
-"radius":0
-},
-{
-"category":37,
-"image":"https://a-maash.com/wp-content/uploads/2021/02/App_Icon-2-300x300.png",
-"radius":0,
-"padding":15
-}
-],
-"isSlider":true
-}
-],
-"Setting":{
-"MainColor":"#3FC1BE",
-"ProductListLayout":"list",
-"StickyHeader":true,
-"ProductDetail":"simpleType"
-},
-"TabBar":[
-{
-"layout":"home",
-"showChat":false,
-"pos":100,
-"icon":"assets/icons/tabs/icon-home.png"
-},
-{
-"categoryLayout":"card",
-"icon":"assets/icons/tabs/icon-category.png",
-"pos":200,
-"layout":"category"
-},
-{
-"icon":"assets/icons/tabs/icon-search.png",
-"pos":300,
-"layout":"search"
-},
-{
-"icon":"assets/icons/tabs/icon-heartthin.png",
-"pos":400,
-"layout":"wishlist"
-},
-{
-"pos":500,
-"showChat":true,
-"layout":"profile",
-"icon":"assets/icons/tabs/icon-user.png"
-}
-],
-"Drawer":{
-"logo":"https://i.imgur.com/18o4YqT.png",
-"items":[
-{
-"show":true,
-"type":"home"
-},
-{
-"type":"web",
-"show":false
-},
-{
-"show":true,
-"type":"about"
-},
-{
-"show":false,
-"type":"login"
-},
-{
-"show":true,
-"type":"category"
-}
-]
-},
-"VerticalLayout":{
-"key":"qsddl5owij",
-"name":"اخر ‏المحاضرات",
-"layout":"list"
-},
-"SplashScreen":{
-"data":"assets/images/splashscreen.png",
-"type":"animated"}}
